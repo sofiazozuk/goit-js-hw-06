18345385216af933d0475c8fc98d96e011c52f8b
Sofia <sofiazozuk@gmail.com>
2023-12-19 16:55:46 +0200

    2d done

--- js/task-2.js
+++ js/task-2.js
@@ -14,5 +14,10 @@ removeItem(itemToRemove){
 }
     
 }
-
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
